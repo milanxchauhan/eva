2f6136a78ad4fd2dd705293560a170d75449445b
Milan Chauhan <milanchauhan.development@gmail.com>
2024-03-16 22:15:34 +0530

    Add Navbar

--- eva-web/src/pages/index.js
+++ eva-web/src/pages/index.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [apiKey, setApiKey] = useState('');
+
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+
   return (
-    <div>Hello World</div>
+    <div className="flex flex-col h-screen">
+      <nav className="shadow p-2 flex flew-row justify-between items-center">
+        <div className="text-xl font-bold">Eva</div>
+        <input 
+        type="password" 
+        className="border p-2 rounded" 
+        onChange={e => setApiKey(e.target.value)}
+        value={apiKey}
+        placeholder="Paste API key here" />
+      </nav>
+    </div>
   );
 }
   
