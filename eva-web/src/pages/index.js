c1cf6d6a30887ebf9fbc7a8b3f62ba23f4284400
Milan Chauhan <milanchauhan.development@gmail.com>
2024-09-02 19:49:36 +0530

    Chat Interface Development

--- eva-web/src/pages/index.js
+++ eva-web/src/pages/index.js
@@ -4,11 +4,22 @@ const SYSTEM_MESSAGE = "You are Eva. An AI agent built by Milan Chauhan using st
 
 export default function Home() {
   const[apiKey, setApiKey] = useState('');
-  const[botMessage, setBotMessage] = useState("");
+  const[messages, setMessages] = useState([
+    { role: "system", content: SYSTEM_MESSAGE },
+  ]);
+  const[userMessage, setUserMessage] = useState("");
 
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
   async function sendRequest() {
+    // update message history
+    const newMessage = { role: "user", content: userMessage};
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+    setUserMessage("");
+
+
     const response = await fetch(API_URL,{
       method: 'POST',
       headers: {
@@ -17,24 +28,21 @@ export default function Home() {
       },
       body: JSON.stringify({
         model: "gpt-3.5-turbo",
-        messages: [
-          // {
-          //   "role": "system",
-          //   "content": "You are a helpful assistant."
-          // },
-          { role: "system", content: SYSTEM_MESSAGE },
-          { role: "user", content: "Hello! Please introduce yourself!" },
-        ],
+        messages: newMessages,
       }),
     });
 
     const responseJson = await response.json();
-    setBotMessage(responseJson.choices[0].message.content);
+    const newBotMessage = responseJson.choices[0].message;
+    const newMessages2 = [...newMessages, newBotMessage];
 
-    console.log("botMessage", botMessage);
+
+    setMessages(newMessages2);
   }
 
   return <div className="flex flex-col h-screen">
+
+    {/* Navigation Bar */}
     <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
       <div className="text-xl font-bold">Eva</div>
       <div>
@@ -47,13 +55,30 @@ export default function Home() {
       </div>
     </nav>
 
-    <div className="p-4">
-      <button 
-      className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white"
-      onClick={sendRequest}>
-        Send Request
-      </button>
-      <div className="text-lg mt-4">{botMessage}</div>
+    {/* Message History */}
+    <div className="flex-1">
+      <div className="w-full max-w-screen-md mx-auto px-4">
+        {messages.filter((message) => message.role !== "system")
+        .map((message, idx) => (
+          <div key={idx} className="mt-3">
+            <div className="font-bold">
+              {message.role === "user" ? "You" : "Eva"}
+            </div>
+            <div className="text-lg">{message.content}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    {/* Message Input Box */}
+    <div>
+      <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
+        <textarea 
+        value={userMessage}
+        onChange={(e) => setUserMessage(e.target.value)}
+        className="border text-lg rounded-md p-1 flex-1" rows={1} />
+        <button onClick={sendRequest} className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-1 ml-2">Send</button>
+      </div>
     </div>
   </div>
 }
